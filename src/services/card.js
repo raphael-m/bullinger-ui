388ffdff14d949cd65be6244bbfac7601e0a1cd5
Raphael Müller <raaphii@gmail.com>
2020-01-09 12:39:35 +0100

    Fixed public path for card service (example)

--- src/services/card.js
+++ src/services/card.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-
+import vueConfig from '../../vue.config'
 
 export default {
     async getCard(id) {
@@ -7,9 +7,9 @@ export default {
             let result;
             // If no id is given, we're in the local development mode - return local example
             if(id)
-                result = await axios.get("/api/assignments/" + id);
+                result = await axios.get(vueConfig.publicPath + "api/assignments/" + id);
             else
-                result = await axios.get("/card-example.json?" + id);
+                result = await axios.get(vueConfig.publicPath + "card-example.json?" + id);
             
             return result.data;
         }
