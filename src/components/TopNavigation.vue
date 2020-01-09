2a8b501bf7bd1bda892e1eb4a596c58654b26bdc
Raphael Müller <raaphii@gmail.com>
2020-01-09 14:42:40 +0100

    Align top navigation to header

--- src/components/TopNavigation.vue
+++ src/components/TopNavigation.vue
@@ -30,6 +30,6 @@ export default {
     .breadcrumb a { color: #212529; padding: $spacing-nav-top-bottom 0; display:inline-block; }
     .breadcrumb-item + .breadcrumb-item::before { content:'>'; font-size:12px; position:relative; top:-2px; }
     .bu-nav-top > a:first-child, .bu-nav-top > a:last-child { padding: $spacing-nav-top-bottom 16px; color: #7C7C7C; }
-    .bu-nav-top > a:first-child { width: 312px; }
+    .bu-nav-top > a:first-child { width: 417px; }
     .bu-nav-top > a:last-child { width: 312px; text-align:right; }
 </style>
